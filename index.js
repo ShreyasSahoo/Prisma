b6566a8cadd55f70cbc6c13a544ff373c137c05e
Shreyas Sahoo <shreyassahoo26@gmail.com>
2023-08-19 18:03:29 +0530

    User Schema added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const express = require("express")
 const app = express()
 app.use(express.json())
 app.listen(3000, () => {
+    console.log(process.env.DATABASE_URL)
     console.log('server running on port 3000')
 })
